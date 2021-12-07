ee64dad046956e33d2f2d741fc399e3a6a1eded8
Dan Dumitriu <dandumitriu33@gmail.com>
2021-12-07 19:35:34 +0200

    Add programatic modal opening button

--- frontend/employeemanagerapp/src/app/app.component.ts
+++ frontend/employeemanagerapp/src/app/app.component.ts
@@ -17,16 +17,35 @@ export class AppComponent implements OnInit {
     this.getEmployees();
   }
 
-public getEmployees(): void {
-  this.employeeService.getEmployees().subscribe(
-    (response: Employee[]) => {
-      this.employees = response;
-      console.log(this.employees);
-    },
-    (error: HttpErrorResponse) => {
-      alert(error.message);
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
     }
-  )
-}
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 
 }
